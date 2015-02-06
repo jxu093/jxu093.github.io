182fc5d84c24ab8404ab76642d6f26cffecf4bdb
jxu093 <jxu093@gmail.com>
2015-02-06 02:09:23 -0500

    Added Scroll effects
    Navigation links light up

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-// $(document).ready(function() {
-// 	$(window).scroll(function() {
-// 		var height = $(window).scrollTop();
-// 		$("#variable").text(height);
-// 	})
-// });
+$(document).ready(function() {
+	$(window).scroll(function() {
+		var winheight = $("body").height();
+		var height = $(window).scrollTop();
+		// $("#nav-contact").text(height);
+		if (height/winheight < 0.05) {
+			$("#nav-home").removeClass("active");
+		}
+		else if (height/winheight < 0.4) {
+			$("#nav-home").addClass("active");
+			$("#nav-resume").removeClass("active");
+		}
+		else if(height/winheight < 0.64) {
+			$("#nav-home").removeClass("active");
+			$("#nav-resume").addClass("active");
+			$("#nav-projects").removeClass("active");
+			$("#nav-contact").removeClass("active");
+		}
+		else {
+			$("#nav-resume").removeClass("active");
+			$("#nav-projects").addClass("active");
+			$("#nav-contact").addClass("active");
+		}
+	})
+});
