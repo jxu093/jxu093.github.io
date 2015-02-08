01fa2e438e248c1c6222053db96df2b986e6d62c
jxu093 <jxu093@gmail.com>
2015-02-08 17:57:42 -0500

    Added Scroll Effects to Navigation
    And deleted unused files

--- old/script.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function() {
-// 	$(function(){
-// 		$(".scroll-pane").jScrollPane();
-// 	});
-		
-// });
--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
-	$(".wrapper").customScrollbar();
 	$(window).scroll(function() {
-		var winheight = $("body").height();
+		var p1 = $("#resume").offset().top*0.95;
+		var p2 = $("#projects").offset().top*0.95;
+		var p3 = $("#contact").offset().top*0.9;
 		var height = $(window).scrollTop();
 		// $("#nav-contact").text(height);
-		if (height/winheight < 0.05) {
-			$("#nav-home").removeClass("active");
-		}
-		else if (height/winheight < 0.4) {
+		if (height < p1) {
 			$("#nav-home").addClass("active");
 			$("#nav-resume").removeClass("active");
+			$("#nav-projects").removeClass("active");
+			$("#nav-contact").removeClass("active");
 		}
-		else if(height/winheight < 0.64) {
+		else if(height < p2) {
 			$("#nav-home").removeClass("active");
 			$("#nav-resume").addClass("active");
 			$("#nav-projects").removeClass("active");
 			$("#nav-contact").removeClass("active");
 		}
-		else {
+		else if(height < p3) {
+			$("#nav-home").removeClass("active");
 			$("#nav-resume").removeClass("active");
 			$("#nav-projects").addClass("active");
+			$("#nav-contact").removeClass("active");
+		}
+		else {
+			$("#nav-home").removeClass("active");
+			$("#nav-resume").removeClass("active");
+			$("#nav-projects").removeClass("active");
 			$("#nav-contact").addClass("active");
 		}
 	})
